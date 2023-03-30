8ada79e11c2f800e5d2c2e0f7feb8a5ba12d2539
hansakam <mhansaka1@gmail.com>
2023-03-30 23:03:24 +0530

    create a loginForm ui

--- src/lk/ijse/hostalmanagementsystem/Appinitializer.java
+++ src/lk/ijse/hostalmanagementsystem/Appinitializer.java
@@ -4,8 +4,27 @@
  */
 package lk.ijse.hostalmanagementsystem;
 
-public class Appinitializer {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.net.URL;
+
+public class Appinitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL resource = getClass().getResource("/lk/ijse/hostalmanagementsystem/view/loginForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Scene scene = new Scene(load);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
