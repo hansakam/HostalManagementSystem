9ecb688bf0a9346ede7310acb53ac76c24cae7be
hansakam <mhansaka1@gmail.com>
2023-03-30 18:28:29 +0530

    now create all main packages

---
+++ src/lk/ijse/hostalmanagementsystem/Appinitializer.java
@@ -0,0 +1,11 @@
+/**
+ * @author :Hansaka Malshan
+ * created 3/30/2023---6:20 PM
+ */
+package lk.ijse.hostalmanagementsystem;
+
+public class Appinitializer {
+    public static void main(String[] args) {
+
+    }
+}
