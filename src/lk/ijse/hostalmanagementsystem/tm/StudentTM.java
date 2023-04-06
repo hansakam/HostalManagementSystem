6521a034a5a0a1c3ad5592b9b3642ea151facf1b
hansakam <mhansaka1@gmail.com>
2023-04-06 22:28:54 +0530

    add a studentTM table

--- src/lk/ijse/hostalmanagementsystem/tm/StudentTM.java
+++ src/lk/ijse/hostalmanagementsystem/tm/StudentTM.java
@@ -4,5 +4,19 @@
  */
 package lk.ijse.hostalmanagementsystem.tm;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class StudentTM {
+    private String sid;
+    private String name;
+    private String address;
+    private String contact;
+    private LocalDate dob;
+    private String gender;
 }
